e689577a7ffe8588a0a692c86b1b0bb500a105e6
alper <alper.usss@gmail.com>
2020-02-22 19:03:55 +0300

    Ecommerce

---
+++ ECommerce/ECommerce/ECommerce.Data/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ECommerce.Data.Entities
+{
+    public class User:Abstracts.Entity
+    {
+        [Required, MinLength(2), MaxLength(50)]
+        public string Name { get; set; }
+        [Required, MinLength(2), MaxLength(50)]
+        public string Surname { get; set; }
+        [Required, MinLength(2), MaxLength(50)]
+        public string Email { get; set; }
+      
+
+        public string Password { get; set; }
+
+    }
+}
---
+++ ECommerce/ECommerce/ECommerce.Data/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECommerce.Data.Interfaces
+{
+    public interface IRepository<T> where T:class
+    {
+        T Insert(T entitiy);
+        T Update(T entitiy);
+        void Purge(int id);
+        IEnumerable<T> List();
+        IQueryable<T> Query();
+        T Get(int id);
+    }
+}
